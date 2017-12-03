d983b77e9affe1bff68cb37202cc90b8071fd7d1
ananthbh6 <ananth@chestnote.com>
2017-12-03 12:04:45 +0100

    Initial Commit

---
+++ What's There?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's There?
+//
+//  Created by Ananth Bhamidipati on 03/12/17.
+//  Copyright © 2017 Ananth Bhamidipati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
