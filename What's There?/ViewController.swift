9e1ae7d8c904e7da750442c7c3abc55bfc294729
ananthbh6 <ananth@chestnote.com>
2017-12-03 12:21:07 +0100

    showing camera

--- What's There?/ViewController.swift
+++ What's There?/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        openCamera()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func openCamera() {
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let inputDevice = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(inputDevice)
+        captureSession.startRunning()
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = self.view.frame
+        self.view.layer.addSublayer(previewLayer)
+    }
 
 }
 
